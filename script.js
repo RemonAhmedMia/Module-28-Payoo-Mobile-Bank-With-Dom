76e1dee15ff72e7b91d78f7bbd15e0188770f894
TheRemonDev <riyanahmed3536@gmail.com>
2025-08-25 19:14:21 +0100

    check the pin number

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 document.getElementById("btn-add-money").addEventListener('click' , function(event){
     event.preventDefault();
 
-    const addMoney = getInputValueById();
-    console.log(addMoney);
+    // const addMoney = getInputValueById();
+    // console.log(addMoney);
+    const addMoney = getInputValueById("add-money");
+    const pinNumber = getInputValueById("pin-number")
     
-})
+    // Wrong Way To Varify. do not try it for your serious WebTransportBidirectionalStream, just for begaier practice
+    if (pinNumber === 1234) {
+        
+    } else{
+        alert("Failed To Add Money");
+    }
+});
--- utilities.js
+++ utilities.js
@@ -1,4 +1,10 @@
-function getInputValueById(){
-    const addMoney = document.getElementById("add-money").vlaue;
-    return addMoney;
-};
+// function getInputValueById(){
+//     const addMoney = document.getElementById("add-money").vlaue;
+//     return addMoney;
+// };
+
+function getInputValueById(id){
+    const inputValue = document.getElementById(id).value;
+    const inputNumber = parseFloat(inputValue);
+    return inputNumber;
+}
