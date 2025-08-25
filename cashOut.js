7c7388519be94b1c3180bf05facd0b6100185902
TheRemonDev <riyanahmed3536@gmail.com>
2025-08-25 19:50:15 +0100

    add-cashout-part-01

---
+++ cashOut.js
@@ -0,0 +1,7 @@
+document.getElementById("btn-cash-out").addEventListener('click' , function(event){
+    event.preventDefault();
+
+    const cashOut = getInputValueById("input-cash-out");
+    const cashOutPin = getInputValueById("cash-out-pin-number");
+    
+});
